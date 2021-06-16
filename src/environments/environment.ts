158d8fd2a0f96dc229feaecd46967ab2d599eda5
cah-samyuktha-koriginja <83737691+cah-samyuktha-koriginja@users.noreply.github.com>
2021-06-16 13:40:09 -0700

    commit test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,18 @@
+// let win=window;
+// let env='';
+// if(win.location.origin=='http://localhost:4200'){
+// console.log("localhost");
+// env=win.location.origin;
+// }
+// else if(win.location.origin=='https://wbd-support-order-placing.apps.cf.np.gc.cahcloud.net'){
+//   console.log("dev url");
+//   env=win.location.origin;
+// }
+
+
 export const environment = {
   production: false,
-  API_URL: ' http://localhost:4200/wbd/order/api/v1/compliancePackageOrder/save'
+  //API_URL: window.location.origin+'/wbd/order/api/v1/compliancePackageOrder/save'
+  //API_URL: 'https://api.dev.cardinalhealth.com/wbd/order/api/v1/compliancePackageOrder/save'
+  API_URL:'https://wbd-support-order-placing.apps.cf.np.gc.cahcloud.net/wbd/order/api/v1/compliancePackageOrder/save'
 };
